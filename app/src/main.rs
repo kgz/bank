08628b76a585edcf2cbb2ec9d31c9cc8878a07e5
Mat <mat.frayne@gmail.com>
2023-02-04 14:48:38 +1000

    jwts working.

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,11 +1,33 @@
 // add mysql
 
 extern crate migrations;
+use actix_jwt_auth_middleware::AuthResult;
+use actix_jwt_auth_middleware::CookieSigner;
+use actix_jwt_auth_middleware::FromRequest;
+use actix_web::HttpServer;
+use actix_web::Responder;
+use actix_web::HttpRequest;
+use actix_web::get;
+use actix_web::http::header::AUTHORIZATION;
+use actix_web::http::header::HeaderMap;
+use jwt_compact::alg::Ed25519;
 use migrations::database::database::Database;
 use migrations::run_migrations;
 use migrations::database::database::DB;
 use migrations::database::database::Ret;
 use migrations::database::database::new;
+use jwt_compact::{prelude::*, alg::{Hs256, Hs256Key}};
+
+use actix_web::{guard, web, App, HttpResponse};
+use routes::auth::Role;
+use routes::auth::authorize;
+use routes::auth::create_jwt;
+use routes::error::Error;
+use serde::Deserialize;
+use serde::Serialize;
+
+use crate::routes::auth::jwt_from_header;
+
 
 mod database {}
 
@@ -38,14 +60,50 @@ fn create_hello()  {
 
 }
 
-#[tokio::main]
-async fn main() {
-    let _db:DB = new().unwrap();
-    run_migrations();
-    routes::routes::gen_routes().await;
-    // let code = user::roles::roles::generate_code();
-    // println!("{}", code);
-    // // get roles
-    // let roles = user::roles::roles::get_roles_from_code(&code);
-    // println!("{:?}", roles);
+// #[tokio::main]
+// async fn main() {
+//     let _db:DB = new().unwrap();
+//     run_migrations();
+//     routes::routes::gen_routes().await;
+//     // let code = user::roles::roles::generate_code();
+//     // println!("{}", code);
+//     // // get roles
+//     // let roles = user::roles::roles::get_roles_from_code(&code);
+//     // println!("{:?}", roles);
+// }
+#[derive(Serialize, Deserialize, Clone, Debug, FromRequest)]
+struct User {
+    id: u32,
+}
+
+
+
+async fn index(req: HttpRequest) -> HttpResponse {
+    let headers = req.headers();
+    let res = authorize((Role::User, headers)).await;
+    // if error 
+    if let Err(e) = res {
+        return HttpResponse::Unauthorized().body(e.to_string());
+    }
+    HttpResponse::Ok().body("Hello")
+}
+
+async fn login (req: HttpRequest) -> HttpResponse {
+    let headers = req.headers();
+    let res = create_jwt("1", &Role::User).await;
+    HttpResponse::Ok().body(format!("Bearer {:?}", res))
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            // .service(testFactory)
+            .route("/", web::get().to(index))
+            .route("/login", web::get().to(login))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
--- app/src/routes/auth.rs
+++ app/src/routes/auth.rs
@@ -1,23 +1,21 @@
-use crate::{routes::error::Error, routes::routes::Result, routes::routes::WebResult};
+use crate::{routes::error::Error};
 
+use actix_web::http::header::{HeaderMap, AUTHORIZATION};
 use chrono::prelude::*;
 use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
 use serde::{Deserialize, Serialize};
 use std::{fmt};
-use warp::{
-    filters::header::headers_cloned,
-    http::header::{HeaderMap, HeaderValue, AUTHORIZATION},
-    reject, Filter, Rejection,
-};
+
 
 const BEARER: &str = "Bearer ";
 const JWT_SECRET: &[u8] = b"secret";
 
-#[derive(Clone, PartialEq)]
+#[derive(Clone, PartialEq, Debug)]
 pub enum Role {
     User,
     Admin,
 }
+
 
 impl Role {
     pub fn from_str(role: &str) -> Role {
@@ -37,6 +35,8 @@ impl fmt::Display for Role {
     }
 }
 
+
+
 #[derive(Debug, Deserialize, Serialize)]
 struct Claims {
     sub: String,
@@ -44,13 +44,13 @@ struct Claims {
     exp: usize,
 }
 
-pub fn with_auth(role: Role) -> impl Filter<Extract = (String,), Error = Rejection> + Clone {
-    headers_cloned()
-        .map(move |headers: HeaderMap<HeaderValue>| (role.clone(), headers))
-        .and_then(authorize)
-}
+// pub fn with_auth(role: Role) -> impl Filter<Extract = (String,), Error = Rejection> + Clone {
+//     headers_cloned()
+//         .map(move |headers: HeaderMap<HeaderValue>| (role.clone(), headers))
+//         .and_then(authorize)
+// }
 
-pub fn create_jwt(uid: &str, role: &Role) -> Result<String> {
+pub async fn create_jwt(uid: &str, role: &Role) -> Result<String, Error> {
     let expiration = Utc::now()
         .checked_add_signed(chrono::Duration::seconds(60))
         .expect("valid timestamp")
@@ -66,29 +66,24 @@ pub fn create_jwt(uid: &str, role: &Role) -> Result<String> {
         .map_err(|_| Error::JWTTokenCreationError)
 }
 
-async fn authorize((role, headers): (Role, HeaderMap<HeaderValue>)) -> WebResult<String> {
-    match jwt_from_header(&headers) {
-        Ok(jwt) => {
-            let decoded = decode::<Claims>(
-                &jwt,
-                &DecodingKey::from_secret(JWT_SECRET),
-                &Validation::new(Algorithm::HS512),
-            )
-            .map_err(|_| reject::custom(Error::JWTTokenError))?;
-            
-            
-
-            if role == Role::Admin && Role::from_str(&decoded.claims.role) != Role::Admin {
-                return Err(reject::custom(Error::NoPermissionError));
-            }
-
-            Ok(decoded.claims.sub)
-        }
-        Err(e) => return Err(reject::custom(e)),
+pub async fn authorize((role, headers): (Role, &HeaderMap)) -> Result<String, Error> {
+    let jwt = jwt_from_header(&headers)?;
+    println!("{:?}", jwt);
+    let token_data = decode::<Claims>(
+        &jwt,
+        &DecodingKey::from_secret(JWT_SECRET),
+        &Validation::new(Algorithm::HS512),
+    )
+    .map_err(|_| Error::JWTTokenError)?;
+    let claims = token_data.claims;
+    if claims.role != role.to_string() {
+        return Err(Error::JWTTokenCreationError);
     }
+    Ok(claims.sub)
+
 }
 
-fn jwt_from_header(headers: &HeaderMap<HeaderValue>) -> Result<String> {
+pub fn jwt_from_header(headers: &HeaderMap) -> Result<String, Error> {
 
     let header = match headers.get(AUTHORIZATION) {
         Some(v) => v,
@@ -98,8 +93,9 @@ fn jwt_from_header(headers: &HeaderMap<HeaderValue>) -> Result<String> {
         Ok(v) => v,
         Err(_) => return Err(Error::NoAuthHeaderError),
     };
-    if !auth_header.starts_with(BEARER) {
-        return Err(Error::InvalidAuthHeaderError);
+    match !auth_header.starts_with(BEARER) {
+        true => return Err(Error::InvalidAuthHeaderError),
+        false => (),
     }
     Ok(auth_header.trim_start_matches(BEARER).to_owned())
 }
